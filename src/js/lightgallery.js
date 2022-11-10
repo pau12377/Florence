9f379abd5329cb1b6887822fa9aa2a9a77bbe279
Namiki <ynamikii07@gmail.com>
2022-11-10 11:46:06 +0900

    feat:floor-planのモジュールをroomに変更し、lightgalleryのjsを作成した

---
+++ src/js/lightgallery.js
@@ -0,0 +1,8 @@
+lightGallery(document.getElementById("lightgallery"), {
+  plugins: [lgZoom, lgThumbnail],
+  speed: 500,
+});
+lightGallery(document.getElementById("lightgallery2"), {
+  plugins: [lgZoom, lgThumbnail],
+  speed: 500,
+});
