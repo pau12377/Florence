9564ab1824302a9f0004d0dd4ccf435d74417979
Namiki <ynamikii07@gmail.com>
2022-11-10 11:50:29 +0900

    feat:Storageを追加した

--- src/js/lightgallery.js
+++ src/js/lightgallery.js
@@ -6,3 +6,7 @@ lightGallery(document.getElementById("lightgallery__living"), {
   plugins: [lgZoom, lgThumbnail],
   speed: 500,
 });
+lightGallery(document.getElementById("lightgallery__storage"), {
+  plugins: [lgZoom, lgThumbnail],
+  speed: 500,
+});
