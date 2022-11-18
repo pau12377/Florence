0ad87361ef3999b2bb9fea1cfd24a0d82990e0d7
Namiki <ynamikii07@gmail.com>
2022-11-18 19:04:12 +0900

    feat:front-pageを追加した

---
+++ template/front-page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>Florence</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+    </header> 
+    <div>Hello World</div>
+    <footer>
+    </footer>
+  </body>
+</html>
