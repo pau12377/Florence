611dc1f0177a085743b798180ef95d6695c896c1
Namiki <ynamikii07@gmail.com>
2022-11-21 18:05:42 +0900

    feat:functions.phpのインデントを修正した

--- template/functions.php
+++ template/functions.php
@@ -1,6 +1,6 @@
 <?php //functions.php
 function enqueue_scripts(){
-  // css
-  wp_enqueue_style('main-css', get_stylesheet_uri());
+// css
+wp_enqueue_style('main-css', get_stylesheet_uri());
 }
-  add_action('wp_enqueue_scripts', 'enqueue_scripts');
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
