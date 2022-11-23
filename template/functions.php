b80a4f40d5439786849c02bd85f684b37877c6bf
Namiki <ynamikii07@gmail.com>
2022-11-23 23:11:12 +0900

    feat:インデントを修正した

--- template/functions.php
+++ template/functions.php
@@ -1,9 +1,9 @@
 <?php //functions.php
-function enqueue_scripts(){
+function enqueue_scripts() {
 // css
-wp_enqueue_style('main-css', get_stylesheet_uri());
+  wp_enqueue_style('main-css', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
 //管理バーの削除
-add_filter( 'show_admin_bar', '__return_false' );
+add_filter('show_admin_bar', '__return_false' );
