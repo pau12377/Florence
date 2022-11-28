1e5f78d8981431fdce925c648cf9c13d2143d54a
Namiki <ynamikii07@gmail.com>
2022-11-28 22:33:57 +0900

    feat:galleryA・Bを移植した

---
+++ template/florenceB_gallery.php
@@ -0,0 +1,173 @@
+<main>
+  <div class="gallery-header">
+    <div class="gallery-header__header">
+      <div class="title">
+        <h2 class="title__main">
+          <span class="title__main--head">G</span>allery
+        </h2>
+      </div>
+    </div>
+    <div class="inner">
+      <p class="gallery-header__lead">
+        全室 南向きなので部屋の中がとても明るく、開放感があります。<br />
+        各扉、床材、サッシなどはこだわりの素材を使用しており、格調高い空間を演出してくれます。<br />
+        室内は白を基調としており、フローリングとドアはナチュラルブラウンでまとめ、キッチンと浴室は深みのあるダークブラウンをアクセントに。<br />
+        ウォークインクローゼットは、約2帖ほどの大容量タイプを採用しております。<br />全室約6mの広いべランダも付いております。<br />
+        防犯設備では、複数台の防犯カメラ、防犯センサー照明が設置されております。<br />複数で使用するため多少の制限はありますが、
+        常時接続・使い放題・WiFi無線LAN54g/bの有料サービスもご用意しております。
+      </p>
+    </div>
+  </div>
+  <div class="gallery">
+    <div class="inner">
+      <ul class="gallery__tab">
+        <li class="gallery__item"><a href="#floor-plan">間取り</a></li>
+        <li class="gallery__item"><a href="#living">リビング</a></li>
+        <li class="gallery__item"><a href="#storage">収納</a></li>
+        <li class="gallery__item"><a href="#kitchen">キッチン</a></li>
+        <li class="gallery__item"><a href="#washroom">洗面所</a></li>
+        <li class="gallery__item"><a href="#bath-toilet">浴室</a></li>
+        <li class="gallery__item"><a href="#entrance">玄関</a></li>
+      </ul>
+    </div>
+    <div id="floor-plan" class="room__back">
+      <div class="inner room__contents">
+        <div class="gallery-title">
+          <h4 class="gallery-title__main">Floor plan</h4>
+          <h5 class="gallery-title__sub">間取り</h5>
+        </div>
+        <div class="room" id="lightgallery__floor-plan">
+          <a class="room__image" href="../src/img/B_floor-plan1.jpg">
+            <h6 class="room__title">1F</h6>
+            <img src="../src/img/B_floor-plan1.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_floor-plan2.jpg">
+            <h6 class="room__title">2F</h6>
+            <img src="../src/img/B_floor-plan2.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="living" class="room__back">
+      <div class="inner room__contents">
+        <div class="gallery-title">
+          <h4 class="gallery-title__main">Living</h4>
+          <h5 class="gallery-title__sub">リビング</h5>
+        </div>
+        <div class="room" id="lightgallery__living">
+          <a class="room__image" href="../src/img/B_Living1.jpg">
+            <img src="../src/img/B_Living1.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Living2.jpg">
+            <img src="../src/img/B_Living2.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Living3.jpg">
+            <img src="../src/img/B_Living3.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Living4.jpg">
+            <img src="../src/img/B_Living4.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Living5.jpg">
+            <img src="../src/img/B_Living5.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Living6.jpg">
+            <img src="../src/img/B_Living6.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="storage" class="room__back">
+      <div class="inner room__contents">
+        <div class="gallery-title">
+          <h4 class="gallery-title__main">Storage</h4>
+          <h5 class="gallery-title__sub">収納</h5>
+        </div>
+        <div class="room" id="lightgallery__storage">
+          <a class="room__image" href="../src/img/B_Storage1.jpg">
+            <img src="../src/img/B_Storage1.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Storage2.jpg">
+            <img src="../src/img/B_Storage2.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Storage2.jpg">
+            <img src="../src/img/B_Storage3.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/B_Storage2.jpg">
+            <img src="../src/img/B_Storage4.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="kitchen" class="room__back">
+      <div class="inner room__contents">
+        <div class="gallery-title">
+          <h4 class="gallery-title__main">Kitchen</h4>
+          <h5 class="gallery-title__sub">キッチン</h5>
+        </div>
+        <div class="room" id="lightgallery__kitchen">
+          <a class="room__image" href="../src/img/A_Kitchen1.jpg">
+            <img src="../src/img/B_Kitchen1.jpg" alt="" />
+          </a>
+          <a class="room__image" href="../src/img/A_Kitchen2.jpg">
+            <img src="../src/img/B_Kitchen2.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="washroom" class="room__back">
+      <div class="inner room__contents">
+        <div class="gallery-title">
+          <h4 class="gallery-title__main">Washroom</h4>
+          <h5 class="gallery-title__sub">洗面所</h5>
+        </div>
+        <div class="room" id="lightgallery__washroom">
+          <a class="room__image" href="../src/img/B_Wash1.jpg">
+            <img src="../src/img/B_Wash1.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div id="bath-toilet" class="room__back">
+      <div class="inner">
+        <div class="room__contents">
+          <div class="gallery-title">
+            <h4 class="gallery-title__main">Bathroom</h4>
+            <h5 class="gallery-title__sub">浴室</h5>
+          </div>
+          <div class="room" id="lightgallery__bathroom">
+            <a class="room__image" href="../src/img/B_Bath1.jpg">
+              <img src="../src/img/B_Bath1.jpg" alt="" />
+            </a>
+            <a class="room__image" href="../src/img/B_Bath2.jpg">
+              <img src="../src/img/B_Bath2.jpg" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="contact">
+    <div class="inner">
+      <div class="contact__wrap">
+        <div class="title">
+          <h2 class="title__main">
+            <span class="title__main--head">C</span>ontact
+          </h2>
+          <h3 class="title__sub">お問い合わせ</h3>
+        </div>
+        <div class="contact__lead">
+          <p>ご相談・ご質問はお電話のみ受け付けております。</p>
+          <p>お気軽に管理人までご連絡ください。</p>
+          <p>
+            また群大生の場合、生協の方が一般の不動産会社より安くご契約できますので、ご検討中の方はご連絡ください。
+          </p>
+        </div>
+        <div class="tel">
+          <h4 class="tel__title">ご相談・ご質問はこちら</h4>
+          <span class="material-icons"> phone </span>
+          <a href="tel:0272355601" class="tel__number">027-235-5601</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
