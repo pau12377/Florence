2356e668059368a863a7b2ad937494ce9735060e
Namiki <ynamikii07@gmail.com>
2022-12-04 23:38:51 +0900

    feat:jsファイルを読み込ませた

--- template/functions.php
+++ template/functions.php
@@ -1,9 +1,12 @@
 <?php //functions.php
 function enqueue_scripts() {
 // css
-  wp_enqueue_style('main-css', get_stylesheet_uri());
+wp_enqueue_style('main-css', get_stylesheet_uri());
+// js
+wp_enqueue_script('jquery-min-js','https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js' , array(), '3.6.1', true );
+wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/style.js' , array('jquery-min-js'), '', true );
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
 //管理バーの削除
-add_filter('show_admin_bar', '__return_false' );
+add_filter( 'show_admin_bar', '__return_false' );
