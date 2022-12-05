e046f9b5361f90adaf98e1c61c2ae6271814500f
Namiki <ynamikii07@gmail.com>
2022-12-05 16:38:51 +0900

    fix:インデント修正

--- template/functions.php
+++ template/functions.php
@@ -1,5 +1,5 @@
 <?php //functions.php
-function enqueue_scripts() {
+function enqueue_scripts(){
   // css
   wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css' , array(), '',);
   wp_enqueue_style('main-css', get_stylesheet_uri());
@@ -7,7 +7,7 @@ function enqueue_scripts() {
   wp_enqueue_script('jquery-min-js', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js' , array(), '3.6.1', true);
   wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js' , array(), '', true );
   wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/style.js' , array('jquery-min-js'), '', true);
-}
+  }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
 //管理バーの削除
