774dea3a6501a9c3f1fabe2953f52700cef82061
Namiki <ynamikii07@gmail.com>
2022-12-31 15:48:45 +0900

    feat:ニュース詳細画面のヘッダーを作成した

--- template/single.php
+++ template/single.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
-
+<div class="news-header">
+  <div class="title">
+    <h2 class="title__main">
+      <span class="title__main--head">N</span>ews
+    </h2>
+  </div>
+</div>
 <?php get_footer(); ?>
